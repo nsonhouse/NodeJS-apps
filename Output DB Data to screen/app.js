68abe2206cb4ddbc51dfbb7591ecb928e0571bc4
nsonhouse <nsonhouse@users.noreply.github.com>
2019-02-12 23:36:17 -0500

    Update app.js

--- Output DB Data to screen/app.js
+++ Output DB Data to screen/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const dbconfig = require("./dbConnection/dbConnection")
 
+const app = express();
+const PORT = 3000;
+
+app.get("/", function(req, res) {
+    let query = "SELECT * FROM Status";
+    connect_and_Query(res, query);
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
